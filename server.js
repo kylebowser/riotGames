fea2311f47bc2ee44a5e703003b68bd0484da311
kylebowser <kylebowser9@gmail.com>
2026-06-10 21:21:48 -0600

    updates to server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path");
 const fetch = require("node-fetch");
 const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/api/player/:name/:tag", async (req, res) => {
   const { name, tag } = req.params;
@@ -18,6 +21,10 @@ app.get("/api/player/:name/:tag", async (req, res) => {
 
   const data = await response.json();
   res.json(data);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
